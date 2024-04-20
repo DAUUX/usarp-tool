45fe0fb836fbcad3c8a790d1538d160b77f733ec
Erikbernard <erikbrsilva@gmail.com>
2024-04-20 00:10:27 -0300

    refactor: back end function

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,19 @@
 import { BrainstormingCard } from "../../components/BrainstormingCard";
 import { ButtonBig } from "../../components/ButtonBig";
+import { Dropdown } from "../../components/DropdownMenu";
+import { useAuth } from "../../hooks/useAuth";
 import styles from "./styles.module.scss";
 
 export function Home() {
+  const { user } = useAuth();
   return (
     <div className={styles.home__container}>
       <section>
-        <div>
+        <div className={styles.home__}>
           <h4>
-            Bem-vindo(a), <span>Mateus Andrade!</span>
+            Bem-vindo(a), <span>{user.fullname}!</span>
           </h4>
+          <Dropdown />
         </div>
         <div className={styles.buttons__container}>
           <ButtonBig label={"Novo projeto"} />
