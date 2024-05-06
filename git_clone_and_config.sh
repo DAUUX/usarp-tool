4755207a6fab0a8b9a1c202f4b6b568cbb598642
erikbernard <eribrsilva@gmail.com>
2024-05-06 12:25:44 -0300

    add env file

---
+++ git_clone_and_config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Carregar variáveis de ambiente do arquivo .env
+if [ -f .env ]; then
+    source .env
+else
+    echo "Arquivo .env não encontrado!"
+    exit 1
+fi
+
+# Configurações do usuário do Git
+#git config --global user.name "usarpback"
+#git config --global user.email "usarpback@gmail.com"
+
+# Clonar o repositório do frontend usando o token para autenticação
+echo "Clonando o repositório do frontend..."
+git clone -b main "https://${GIT_TOKEN}@${FRONTEND_REPO:8}" frontend
+
+# Clonar o repositório do backend usando o token para autenticação
+echo "Clonando o repositório do backend..."
+git clone -b main "https://${GIT_TOKEN}@${BACKEND_REPO:8}" backend
+
+echo "Repositórios clonados com sucesso!"
