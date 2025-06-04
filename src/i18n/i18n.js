a9b1f81c1469a9cc4b8dd301a01a2306c574c7c6
erikbernard <erikbrsilva@gmail.com>
2025-06-03 22:54:09 -0300

    feat: setar o idioma do navegador caso não tenha idioma padrão

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -13,16 +13,30 @@ const resources = {
   }
 };
 
+const getBrowserLanguage = () => {
+  const storedLang = localStorage.getItem("@usarp:language");
+  
+  if (storedLang && ["pt", "en"].includes(storedLang)) {
+    return storedLang;
+  }
+  const browserLang = navigator.language || navigator.userLanguage;
+  const shortLang = browserLang.split("-")[0];
+  const defaultLang = ["pt", "en"].includes(shortLang) ? shortLang : "pt";
+  localStorage.setItem("@usarp:language", defaultLang);
+  
+  return defaultLang;
+};
+
 i18n
   .use(initReactI18next) // passa o i18n para react-i18next.
   .init({
     resources,
-    lng: "pt", // Idioma inicial
+    lng: getBrowserLanguage(), // Idioma inicial
     fallbackLng: "pt", // Caso o idioma não seja encontrado
 
     interpolation: {
-      escapeValue: false // React já faz escaping de valores por padrão
-    }
+      escapeValue: false, // React já faz escaping de valores por padrão
+    },
   });
 
 export default i18n;
