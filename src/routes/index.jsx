5ae983cf5233748d981aee8501a3a0cc701e15e1
douglas de lima bezerra <douglaslima13bezerra@gmail.com>
2025-12-05 21:18:37 -0300

    refactor: replaces the loading text with a CircularProgress component

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,18 @@
 import { useAuth } from "../hooks/useAuth";
 import { LoggedRoutes } from "./LoggedRoutes";
 import { UnloggedRoutes } from "./UnloggedRoutes";
-// import LoadingSpinner from "../components/ui/LoadingSpinner"; // Opcional: componente de loading
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 export default function Routes() {
-const { signed, isLoading } = useAuth();
+  const { signed, isLoading } = useAuth();
 
   if (isLoading) {
-    return <div>Carregando...</div>;
+    return (
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress color="success" size="3rem" />
+      </Box>
+    );
   }
 
   return signed ? <LoggedRoutes /> : <UnloggedRoutes />;
